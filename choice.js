105435efe6f6bd2101de411b13ad6b8262a0898e
guilhermedd <guiddiel@gmail.com>
2021-09-23 00:37:51 -0300

    Funcional
    site funcionando

--- choice.js
+++ choice.js
@@ -1,11 +1,17 @@
-let codigoFinal = ['a','b','c']
-
 function clicar(c) {
-    codigoFinal.push(c);
-    localStorage.setItem('codigoFinal', codigoFinal);
+  let codigoFinal = pegarCodigoFinal();
+  codigoFinal.push(c);
+  console.log(codigoFinal);
+  localStorage.setItem('codigoFinal', JSON.stringify(codigoFinal));
 }
 
 function pegarCodigoFinal() {
-  return localStorage.getItem('codigoFinal', codigoFinal);
-  // console.log(localStorage.getItem('codigoFinal'));
+  let atual = localStorage.getItem('codigoFinal');
+
+  if (!atual) {
+    // console.log('Chegou aqui!')
+    return []
+  }
+
+  return JSON.parse(atual);
 }
