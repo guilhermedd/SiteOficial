77c9289b81b4040fc42f8bae9a2286c25f798a2f
guilhermedd <guiddiel@gmail.com>
2021-09-28 17:27:50 -0300

    4Pagina

--- choice.js
+++ choice.js
@@ -5,13 +5,25 @@ function clicar(c) {
   localStorage.setItem('codigoFinal', JSON.stringify(codigoFinal));
 }
 
+function excluir() {
+  let codigoFinal = pegarCodigoFinal();
+  codigoFinal.pop();
+  console.log(codigoFinal);
+  localStorage.setItem('codigoFinal', JSON.stringify(codigoFinal));
+}
+
 function pegarCodigoFinal() {
   let atual = localStorage.getItem('codigoFinal');
 
   if (!atual) {
-    // console.log('Chegou aqui!')
-    return []
+    console.log('Chegou aqui!');
+    return [];
   }
 
-  return JSON.parse(atual);
+  try {
+    return JSON.parse(atual)
+  } catch {
+    return [];
+  }
+
 }
