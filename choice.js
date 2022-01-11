d12a2ba487e009db43e3f088fa85d9e4f78bf6f8
MatSchiochet <schiochet.m@gmail.com>
2022-01-11 16:44:23 -0300

    ajuste do sintetico
    uehueheuehue coisa pra carambaaa

--- choice.js
+++ choice.js
@@ -65,7 +65,10 @@ function final() {
   } else if (codigo[1] == 'NB') {
     codigo.push('4')
   }
-  final.innerHTML = codigo.join('');
+  let codigo_final = separate(codigo,4)
+
+  
+  final.innerHTML = codigo_final.join('');
 }
 
 function final2() {
@@ -90,7 +93,10 @@ function final2() {
   codigo[5] = codigo[6];
   codigo.pop();
   
-  final.innerHTML = codigo.join('');
+  let codigo_final = separate(codigo,3)
+
+  
+  final.innerHTML = codigo_final.join('');
 }
 
 
@@ -109,23 +115,70 @@ function colocazero() {
 function final3() {
   const final = document.getElementById('Final')
   const codigo = JSON.parse(localStorage.getItem('codigoFinal'))
-  let i = 4;
-  if (codigo[4] == '6') i++
-  if (codigo[2] == ' ') {
-    codigo.splice(2,1)
-  // } else if (codigo[i] == '4') {
-  //   codigo.push('4')
-  // } else if (codigo[i] == '1') {
-  //   codigo.push('1')
-  // } else if (codigo[i] == '3') {
-  //   codigo.push('3')
-  // } else if (codigo[i] == '6') {
-  //   codigo.push('6')
-  // } else if (codigo[i] == '7') {
-  //   codigo.push('7')
-  // } else if (codigo[i] == '8') {
-  //   codigo.push('8')
+  if (codigo[2] == ' ') 
+   {
+      codigo.splice(2,1)
+     if (codigo[3] == '31P') {
+       codigo[3] = '31' + codigo[4] + 'P'
+        codigo.pop()
+      }
+      if (codigo[3] == '25K') {
+        codigo[3] = '25' + codigo[4] + 'K'
+       codigo.pop()
+     }
+     if (codigo[3] == '21K') {
+       codigo[3] = '21' + codigo[4] + 'K'
+       codigo.pop()
+      }
+      if (codigo[3] == '31T') {
+        codigo[3] = '31' + codigo[4] + 'T'
+        codigo.pop()
+      }
+      let codigo_final = separate(codigo,3)
+
+  
+  final.innerHTML = codigo_final.join('');
+    } else {
+      if (codigo[4] == '31P') {
+        codigo[4] = '31' + codigo[5] + 'P'
+         codigo.pop()
+       }
+       if (codigo[4] == '25K') {
+         codigo[4] = '25' + codigo[5] + 'K'
+        codigo.pop()
+      }
+      if (codigo[4] == '21K') {
+        codigo[4] = '21' + codigo[5] + 'K'
+        codigo.pop()
+       }
+       if (codigo[4] == '31T') {
+         codigo[4] = '31' + codigo[5] + 'T'
+         codigo.pop()
+       }
+       let codigo_final = separate(codigo,4)
+
+  
+  final.innerHTML = codigo_final.join('');
+    }
+  //let codigo_final = separate(codigo,4)
+
+  
+  //final.innerHTML = codigo_final.join('');
+  // final.innerHTML = separate(codigo,4).join('');
+}
+
+function separate(lista, indice) {
+  let codigo = []
+  for(let i = 0; i < indice; i++) {
+    codigo[i] = lista[i]
   }
   
-  final.innerHTML = codigo.join('');
+  codigo.push(' ')
+
+  for(let i = indice + 1; i <= lista.length; i++) {
+    codigo[i] = lista[i - 1]
+  }
+  
+  return codigo
+  
 }
