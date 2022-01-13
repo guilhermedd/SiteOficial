5e3cb29a737247720360f568c597755721d41667
MatSchiochet <schiochet.m@gmail.com>
2022-01-13 15:56:11 -0300

    FINALIZADO COM FAROFA
    asdasdasd

--- choice.js
+++ choice.js
@@ -77,47 +77,29 @@ function final2() {
   if (codigo[3] == '25K') {
     codigo[3] = '25' + codigo[4] + 'K'
     codigo.pop()
-   } else {
-    if (codigo[4] == '5') {
-      codigo.push('5')
-    } else if (codigo[4] == '4') {
-      codigo.push('4')
-    } else if (codigo[4] == '1') {
-      codigo.push('1')
-    } else if (codigo[4] == '3') {
-      codigo.push('3')
-    } else if (codigo[4] == '6') {
-      codigo.push('6')
-    } else if (codigo[4] == '7') {
-      codigo.push('7')
-    } else if (codigo[4] == '8') {
-      codigo.push('8')
-    }
+   }  
+   if (codigo[4] != '0') {
+     let aux = codigo[4];
+     codigo[4] = codigo[5];
+     codigo[5] = aux;
    }
-
-  codigo[4] = codigo[5];
-  codigo[5] = codigo[6];
-  codigo.pop();
-  
-  //let codigo_final = separate(codigo,3)
-
-  
-  //final.innerHTML = codigo_final.join('');
-  final.innerHTML = codigo.join('');
+       
+  const codigo_final = separate(codigo,3)
+  final.innerHTML = codigo_final.join('');
 }
 
 
-function tirazero() {
-  let codigoFinal = pegarCodigoFinal();
-  codigoFinal[3] = '6';
-  localStorage.setItem('codigoFinal', JSON.stringify(codigoFinal));
-}
+// function tirazero() {
+//   let codigoFinal = pegarCodigoFinal();
+//   codigoFinal[3] = '6';
+//   localStorage.setItem('codigoFinal', JSON.stringify(codigoFinal));
+// }
 
-function colocazero() {
-  let codigoFinal = pegarCodigoFinal();
-  codigoFinal[3] = '60';
-  localStorage.setItem('codigoFinal', JSON.stringify(codigoFinal));
-}
+// function colocazero() {
+//   let codigoFinal = pegarCodigoFinal();
+//   codigoFinal[3] = '60';
+//   localStorage.setItem('codigoFinal', JSON.stringify(codigoFinal));
+// }
 
 function final3() {
   const final = document.getElementById('Final')
@@ -167,10 +149,10 @@ function final3() {
   
   final.innerHTML = codigo_final.join('');
     }
-  //let codigo_final = separate(codigo,4)
+  let codigo_final = separate(codigo,4)
 
   
-  //final.innerHTML = codigo_final.join('');
+  final.innerHTML = codigo_final.join('');
   // final.innerHTML = separate(codigo,4).join('');
 }
 
@@ -188,4 +170,10 @@ function separate(lista, indice) {
   
   return codigo
   
+}
+
+function copy() {
+  const copyText = document.getElementById('Final').innerHTML
+  const cb = navigator.clipboard;
+  cb.writeText(copyText)
 }
